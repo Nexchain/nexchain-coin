877f449d516fe79ee6e221cc4fbd55428db24295
Nexchain Support <support@nexchain.ai>
2025-04-18 14:11:38 +0200

    Adjust deployment script, add basic README

--- script/NexchainCoin.s.sol
+++ script/NexchainCoin.s.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.20;
 import {Script, console} from "lib/forge-std/src/Script.sol";
 import {NexchainCoin} from "../src/NexchainCoin.sol";
 
-contract DeployScript is Script {
+contract NexchainCoinScript is Script {
     function run() external {
-        // Get the private key from the environment
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddress = vm.addr(deployerPrivateKey);
         address daoMultisig = vm.envAddress("DAO_MULTISIG");
         
         // Start broadcasting transactions
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
         
         // Deploy the contract
         NexchainCoin token = new NexchainCoin(daoMultisig);
@@ -21,6 +18,6 @@ contract DeployScript is Script {
         vm.stopBroadcast();
         
         // Log the deployed address
-        console2.log("NexchainCoin deployed at:", address(token));
+        console.log("NexchainCoin deployed at:", address(token));
     }
 } 
