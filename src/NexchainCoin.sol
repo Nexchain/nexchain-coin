549314b3b4aa183a809a64470c5aa7632171e990
Nexchain Support <support@nexchain.ai>
2025-04-18 12:38:48 +0200

    Remove .vscode from git history, ignore .vscode, adjust NexchainCoin.sol to not rely on block timestamp

--- src/NexchainCoin.sol
+++ src/NexchainCoin.sol
@@ -17,42 +17,40 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract NexchainCoin is ERC20, ERC20Permit, ERC20Votes, Ownable {
-    event OpenBurn(uint256 burnPossibleFromTime, uint256 amount);
+    event OpenBurn(uint256 burnPossibleFromBlock, uint256 amount);
 
-    uint256 public burnPreparationDuration = 2 * 24 * 60 * 60;
+    // Assuming average block time of 12 seconds, this gives approximately 2.5 days
+    uint256 private constant BURN_TIMELOCK_BLOCKS = 18000; // 2.5 * 24 * 60 * 60 / 12
 
-    uint256 private _burnPossibleFromTime;
+    uint256 private _burnPossibleFromBlock;
 
     constructor(address wallet) ERC20("Nexchain", "NEX") ERC20Permit("Nexchain") Ownable(wallet) {
         _mint(wallet, 2150000000000000000000000000);
     }
 
-    function burnInfo() public view returns (uint256 possibleFromTime, uint256 amount) {
-        if (_burnPossibleFromTime != 0) {
-            possibleFromTime = _burnPossibleFromTime;
+    function burnInfo() public view returns (uint256 possibleFromBlock, uint256 amount) {
+        if (_burnPossibleFromBlock != 0) {
+            possibleFromBlock = _burnPossibleFromBlock;
             amount = balanceOf(address(this));
         }
     }
 
     function openBurn(uint256 amount) external onlyOwner {
-        require(_burnPossibleFromTime == 0, "Already open");
-
-        require(amount != 0, "Amount cannot be 0");
-
-        require(amount <= balanceOf(msg.sender), "Not enough tokens");
+        require(_burnPossibleFromBlock == 0, "Burn process already in timelock phase");
+        require(amount != 0, "Amount to burn cannot be 0");
+        require(amount <= balanceOf(msg.sender), "Not enough tokens to burn");
 
         transfer(address(this), amount);
-        _burnPossibleFromTime = block.timestamp + burnPreparationDuration;
-        emit OpenBurn(_burnPossibleFromTime, amount);
+        _burnPossibleFromBlock = block.number + BURN_TIMELOCK_BLOCKS;
+        emit OpenBurn(_burnPossibleFromBlock, amount);
     }
 
     function finishBurn() external {
-        require(_burnPossibleFromTime != 0, "Not open");
-
-        require(_burnPossibleFromTime <= block.timestamp, "Time has not come yet");
+        require(_burnPossibleFromBlock != 0, "Burn process was not initiated");
+        require(_burnPossibleFromBlock <= block.number, "Burn process is still in timelock phase");
 
         _burn(address(this), balanceOf(address(this)));
-        _burnPossibleFromTime = 0;
+        _burnPossibleFromBlock = 0;
     }
 
     function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
